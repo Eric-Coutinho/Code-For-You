2b6fe5b29bcf06d5db70e8cf2c44e1653afe51ae
Eric Coutinho <eric.coutinhop@hotmail.com>
2024-05-03 15:18:11 -0300

    Starting to work on cards components

---
+++ front/src/Components/Cards/index.js
@@ -0,0 +1,10 @@
+import styles from "./styles.module.scss"
+
+export default function Cards({ title, description }) {
+    return(
+        <>
+            {title}, {description}
+        </>
+    )
+}
+
