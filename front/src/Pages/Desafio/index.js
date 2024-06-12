ce48748a5a17a86281860934c042c30e4686cbfd
eric-coutinho <eric.coutinhop@hotmail.com>
2024-06-12 10:14:06 -0300

    Small changes on the challenge screen

--- front/src/Pages/Desafio/index.js
+++ front/src/Pages/Desafio/index.js
@@ -1,7 +1,21 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import styles from "./styles.module.scss"
+
 export function DesafioPage() {
-    return(
+    return (
         <>
-            Desafio works.
+            <Row className={styles.center} style={{ marginTop: '2em' }}>
+                <Col>
+                </Col>
+                <Col lg='4' md='6' sm='12' className={styles.center}>
+                    <div className={styles.title}>
+                        Desafio 1
+                    </div>
+                </Col>
+                <Col></Col>
+            </Row>
         </>
     )
 }
